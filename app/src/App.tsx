f9362ed4294965dceb78fd3de24b5d865873710f
Elias ZANOTTI <zanotti.elias@gmail.com>
2026-01-14 15:35:42 +0100

    add shadcn

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Landing } from './components/Landing';
-import { GameRoomClient } from './components/GameRoomClient';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from "./components/Landing";
+import { GameRoomClient } from "./components/GameRoomClient";
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/:room/:playerName" element={<GameRoomClient />} />
-            </Routes>
-        </BrowserRouter>
-    );
+export function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="/:room/:playerName" element={<GameRoomClient />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
-
-export default App;
